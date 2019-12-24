88cec73491d17c4c3fe40ce0c9b8e91d9d9d25cf
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-12-24 05:11:55 +0000

    aex-ify

--- aosp_oxygen.mk
+++ aosp_oxygen.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/oxygen/device.mk)
 
 # Inherit some common aosp  stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Gapps config
 TARGET_GAPPS_ARCH := arm64
