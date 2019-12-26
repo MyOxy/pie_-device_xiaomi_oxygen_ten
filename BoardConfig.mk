a8707ca99ae52154649bb487a5154af687af605e
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-12-26 15:20:11 +0000

    oxygen: sepolicy: Switch to qcom legacy sepolicy
    * android-10 build with sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -231,7 +231,9 @@ TARGET_USE_SDCLANG := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # Sepolicy
-BOARD_SEPOLICY_VERS := 28.0
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # SHIM
 TARGET_LD_SHIM_LIBS := \
