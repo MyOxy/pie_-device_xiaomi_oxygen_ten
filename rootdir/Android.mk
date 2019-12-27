9575f695bd8e03360e71e400ba12ac98699f62e7
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-12-27 11:10:13 +0000

    oxygen: rootdir: fixup typo

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -80,7 +80,9 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/service.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
 
+include $(BUILD_PREBUILT)
 LOCAL_MODULE	   := move_time_data.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
